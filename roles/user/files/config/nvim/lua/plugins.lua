df61f59062fd860ed254f6feefede6463d083700
nightusk <nightusk@gmail.com>
2023-12-30 19:43:08 +0900

    fix some errors and lsp config

--- roles/user/files/config/nvim/lua/plugins.lua
+++ roles/user/files/config/nvim/lua/plugins.lua
@@ -51,7 +51,10 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require('lspconfig')
+      lspconfig.ccls.setup{}
       lspconfig.denols.setup{}
+      lspconfig.rust_analyzer.setup{}
+      lspconfig.zls.setup{}
     end
   }, -- }}}
 
